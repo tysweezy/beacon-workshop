b0aed0d7d6502dcaec5850f46c3db6feff4c2fb7
tysweezy <tsouzs.sf@gmail.com>
2014-07-08 08:34:00 -0700

    Slideshow and other milestones

---
+++ js/site.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+  	nav: true,
+  	slideSpeed: 300,
+  	paginationSpeed: 400,
+  	items: 1,
+  	rewindNav: false,
+  	stopOnHover: true,
+  	autoPlay: 7000
+  });
+});
