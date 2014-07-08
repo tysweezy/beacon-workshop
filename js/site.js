3904837d045c114b843e01188f06e0fdcc5eb897
tysweezy <tsouzs.sf@gmail.com>
2014-07-08 12:17:05 -0700

    Quote functionality finished.

--- js/site.js
+++ js/site.js
@@ -9,4 +9,45 @@ $(document).ready(function(){
   	autoplay: 7000,
   	navText: ['&#x2190;','&#8594;']
   });
+
+
+
+ $('#jwt-quote').show();
+  $('#best-quote').hide();
+  $('#dish-quote').hide();
+  $('#yougov-quote').hide();
+  
+
+ $('#jwt').click(function(){
+   $('#jwt-quote').show();
+  $('#best-quote').hide();
+  $('#dish-quote').hide();
+  $('#yougov-quote').hide();
+  
+ });
+  
+ $('#bestwestern').click(function(){
+   $('#best-quote').fadeIn();
+    $('#dish-quote').hide();
+  	$('#yougov-quote').hide();
+  	$('#jwt-quote').hide();
+
+
+ });
+  
+ $('#dish').click(function(){
+   $('#dish-quote').fadeIn();
+  	$('#yougov-quote').hide();
+  	$('#jwt-quote').hide();
+  	$('#best-quote').hide();
+
+ });
+
+  $('#yougov').click(function(){
+   $('#dish-quote').hide();
+  	$('#yougov-quote').fadeIn();
+  	$('#jwt-quote').hide();
+  	$('#best-quote').hide();
+
+ });
 });
